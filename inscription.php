074da56de001ae4e445978d8f4e413a58cf5696e
SachaB96 <sacha0007@msn.com>
2017-04-19 16:22:45 +0200

    Test insertion

--- inscription.php
+++ inscription.php
@@ -10,18 +10,17 @@ $firstname = filter_input(INPUT_POST, 'firstname');
 $lastname = filter_input(INPUT_POST, 'lastname');
 $pseudo = filter_input(INPUT_POST, 'pseudo');
 $pass = filter_input(INPUT_POST, 'pass');
-$phone = filter_input(INPUT_POST, 'phone');
+//$phone = filter_input(INPUT_POST, 'phone');
 $mail = filter_input(INPUT_POST, 'mail');
-$emergency_mail = filter_input(INPUT_POST, 'emergency_mail');
-$comments = filter_input(INPUT_POST, 'comments');
+//$emergency_mail = filter_input(INPUT_POST, 'emergency_mail');
+//$comments = filter_input(INPUT_POST, 'comments');
 
 $pass_length = strlen($pass);
 
 
 /* Si le formulaire est envoyé */
-if (isset($firstname, $lastname, $pseudo, $pass, $phone, $mail, $emergency_mail, $comments)) {   
+if (isset($firstname, $lastname, $pseudo, $pass, $mail)) {   
     if(filter_var($mail, FILTER_VALIDATE_EMAIL) && filter_var($emergency_mail, FILTER_VALIDATE_EMAIL)){
-        if($mail != $emergency_mail){
             if($pass_length > 4){
 
             /* aene que les valeurs ne sont pas vides ou composées uniquement d'espaces  */ 
@@ -29,13 +28,13 @@ if (isset($firstname, $lastname, $pseudo, $pass, $phone, $mail, $emergency_mail,
             $lastname = trim($lastname) != '' ? $lastname : null;
             $pseudo = trim($pseudo) != '' ? $pseudo : null;
             $pass = trim($pass) != '' ? $pass : null;
-            $phone = trim($phone) != '' ? $phone : null;
+            //$phone = trim($phone) != '' ? $phone : null;
             $mail = trim($mail) != '' ? $mail : null;
-            $emergency_mail = trim($emergency_mail) != '' ? $emergency_mail : null;
-            $comments = trim($comments) != '' ? $comments : null;
+           // $emergency_mail = trim($emergency_mail) != '' ? $emergency_mail : null;
+            //$comments = trim($comments) != '' ? $comments : null;
 
                 /* Si les champs sont différents de null */
-                if(isset($lastname, $firstname, $pseudo, $pass, $mail, $emergency_mail)) {
+                if(isset($lastname, $firstname, $pseudo, $pass, $mail)) {
                     /* Connexion au serveur : dans cet exemple, en local sur le serveur d'évaluation
                     A MODIFIER avec vos valeurs */
                     $hostname = "localhost";
@@ -79,13 +78,13 @@ if (isset($firstname, $lastname, $pseudo, $pass, $phone, $mail, $emergency_mail,
                             /* Résultat du comptage = 0 pour ce pseudo, on peut donc l'enregistrer */
                         
                             /* Pour enregistrer la date actuelle (date/heure/minutes/secondes) on peut utiliser directement la fonction mysql : NOW()*/
-                            $insertion = "INSERT INTO user(firstname, lastname, pseudo,pass, phone, mail, emergency_mail, comments, registration_date) VALUES(:firstname, :lastname, :nom, :password, :phone, :mail, :emergency_mail, :comments, NOW())";
+                            $insertion = "INSERT INTO user(firstname, lastname, pseudo,pass, mail, registration_date) VALUES(:firstname, :lastname, :nom, :password, :mail, NOW())";
                             
                             /* préparation de l'insertion */
                             $insert_prep = $connect->prepare($insertion);
                             
                             /* Exécution de la requête en passant les marqueurs et leur variables associées dans un tableau*/
-                            $inser_exec = $insert_prep->execute(array(':firstname'=>$firstname, ':lastname'=>$lastname, ':nom'=>$pseudo,':password'=>$pass, ':phone'=>$phone, ':mail'=>$mail, ':emergency_mail'=>$emergency_mail, ':comments'=>$comments));
+                            $inser_exec = $insert_prep->execute(array(':firstname'=>$firstname, ':lastname'=>$lastname, ':nom'=>$pseudo,':password'=>$pass, ':mail'=>$mail));
                             
                             /* Si l'insertion s'est faite correctement...*/
                             if ($inser_exec === true) {
@@ -96,8 +95,8 @@ if (isset($firstname, $lastname, $pseudo, $pass, $phone, $mail, $emergency_mail,
                                 /* A MODIFIER Remplacer le '#' par l'adresse de votre page de destination, sinon ce lien indique la page actuelle.*/
                                 $message = 'Votre inscription est bien enregistrée.';
                                 /*ou redirection vers une page en cas de succès ex : menu.php*/
-                                /*header("Location: menu.php");
-                                    exit();  */
+                                header("Location: index.html");
+                                    exit();  
                             }   
                         }else{   /* Le pseudo est déjà utilisé */
                             $message = 'Ce pseudo existe déjà, Veuillez en choisir un autre.';
@@ -119,97 +118,5 @@ if (isset($firstname, $lastname, $pseudo, $pass, $phone, $mail, $emergency_mail,
     }else{
         $message = 'Le format de l\'adresse mail est incorrect';
     }
-}
+
 ?>
-<!doctype html>
-<html lang="fr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Formulaire d'inscription - tutoriel PHP France</title>
-
-<style type="text/css">
-<!--
-body, p, h1,form, input, fieldset 
-{
-  margin:0;
-  padding:0;
-}
-
-body 
-{
-  background-color: #F4F4F4;
-}
-
-#inscription 
-{
-  width:450px;
-  background:#FFFFFF;
-  margin:20px auto;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size:1em;
-  border:1px solid #ccc;
-  border-radius:10px;
-}
-
-#inscription fieldset 
-{
-  text-align:center;
-  font-size:1.2em;
-  background:#333333;
-  padding-bottom:5px;
-  margin-bottom:15px;
-  color:#FFFFFF;
-  letter-spacing:0.05em;
-  border-top-left-radius:10px;
-  border-top-right-radius:10px;
-}
-
-#inscription p 
-{
-  padding-top:15px;
-  padding-right:50px;
-  text-align:right;
-}
-
-#inscription input 
-{
-  margin-left:30px;
-  width:150px;
-}
-
-#inscription #valider 
-{
-  width:155px;
-  font-size:0.8em;
-}
-
-#inscription #message 
-{
-  height:27px;
-  color:#F00;
-  font-size:0.8em;
-  font-weight:bold;
-  text-align:center;
-  padding:10px 0 0 0;
-}
--->
-</style>
-</head>
-<body>
-<div id = "inscription">
-    <form action = "#" method = "post">
-    <fieldset>Inscription</fieldset>
-    <p><label for = "firstname">Nom (*) : </label><input type = "text" name = "firstname" id = "firstname" /></p>
-    <p><label for = "lastname">Prénom (*) : </label><input type = "text" name = "lastname" id = "lastname" /></p>
-    <p><label for = "pseudo">Pseudo (*) : </label><input type = "text" name = "pseudo" id = "pseudo" /></p>
-    <p><label for = "pass">Mot de passe (*) : </label><input type = "password" name = "pass" id = "pass" /></p>
-    <p><label for = "phone">N° téléphone (*) : </label><input type = "text" name = "phone" id = "phone" /></p>
-    <p><label for = "mail">Adresse mail (*) : </label><input type = "text" name = "mail" id = "mail" /></p>
-    <p><label for = "emergency_mail">Adresse mail de secours (*) : </label><input type = "text" name = "emergency_mail" id = "emergency_mail" /></p>
-    <p><label for = "comments">Commentaires : </label><input type = "text" name = "comments" id = "comments" /></p>
-    <p><input type = "submit" value = "Envoyer" id = "valider" /></p>
-    </form>
-    <p id = "message"><?= $message?:'' ?></p>
-</div>
-</body>
-</html>
