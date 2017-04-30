9510ade3e50fd35aa96f788c4c53a70374494269
lukile <lucile.1988.ls@gmail.com>
2017-05-01 00:30:04 +0200

    Added hour and date in reservation form

--- reservations.php
+++ reservations.php
@@ -14,7 +14,23 @@
     $aviDate = filter_input(INPUT_POST, 'aviDate');
     $aviHeure = filter_input(INPUT_POST, 'aviHeure');
 
+    $attDate = filter_input(INPUT_POST, 'attDate');
+    $attHeure = filter_input(INPUT_POST, 'attHeure');
 
+    $netDate = filter_input(INPUT_POST, 'netDate');
+    $netHeure = filter_input(INPUT_POST, 'netHeure');
+
+    $paraDate = filter_input(INPUT_POST, 'paraDate');
+    $paraHeure = filter_input(INPUT_POST, 'paraHeure');
+
+    $ulmDate = filter_input(INPUT_POST, 'ulmDate');
+    $ulmHeure = filter_input(INPUT_POST, 'ulmHeure');
+
+    $baptDate = filter_input(INPUT_POST, 'baptDate');
+    $baptHeure = filter_input(INPUT_POST, 'baptHeure');
+
+    $leconDate = filter_input(INPUT_POST, 'leconDate');
+    $leconHeure = filter_input(INPUT_POST, 'leconHeure');
 
 //Récupération des valeurs des options du select
 if(isset($_POST['planeSelecter']) && isset($_POST['fuel']) && isset($_POST['category']) && isset($_POST['acousticGroup'], $planeLength, $maxWeight)){
@@ -26,7 +42,7 @@ if(isset($_POST['planeSelecter']) && isset($_POST['fuel']) && isset($_POST['cate
     $planeLength = trim($planeLength) != '' ? $planeLength : null;
     $maxWeight = trim($maxWeight) != '' ? $maxWeight : null;
 
-    //if(isset($plane, $fuel, $category, $acousticGroup, $planeLength, $maxWeight)){
+    if(isset($plane, $fuel, $category, $acousticGroup, $planeLength, $maxWeight)){
         if(isset($plane, $fuel, $category)){
             $hostname = "localhost";
             $database = "aen";
@@ -59,22 +75,40 @@ if(isset($_POST['planeSelecter']) && isset($_POST['fuel']) && isset($_POST['cate
                   $inser_exec_services = $insert_prep_services->execute(array(':service'=>$service));
             }
 
-             $insertion = "INSERT INTO royalties(landing_type, petroleum_type, rate_type, plane_length, plane_weight) VALUES(:plane, :fuel, :category, :planeLength, :maxWeight)";
+            $insertion = "INSERT INTO royalties(landing_type, petroleum_type, rate_type, plane_length, plane_weight) VALUES(:plane, :fuel, :category, :planeLength, :maxWeight)";
             $insertion_acousticGroup = "INSERT INTO coefficient(acoustic_group) VALUES(:acousticGroup)";
 
 
             $insertion_dateHourParking = "INSERT INTO order_form_service(booking_start_date, booking_end_date, booking_start_hour, booking_end_hour) VALUES(:startDate, :endDate, :startHour, :endHour)";
-            $insesrtion_dateHourAvi = "INSERT INTO order_form_service(booking_start_date, booking_end_date) VALUES(:aviDate, :aviHeure)";
+            $insertion_dateHourAvi = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES(:aviDate, :aviHeure)";
+            $insertion_dateHourAtt = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES(:attDate, :attHeure)";
+            $insertion_dateHourNet = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES (:netDate, :netHeure)";
+            $insertion_dateHourPara = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES(:paraDate, :paraHeure)";
+            $insertion_dateHourUlm = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES(:ulmDate, :ulmHeure)";
+            $insertion_dateHourBapt = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES (:baptDate, :baptHeure)";
+            $insertion_dateHourLecon = "INSERT INTO order_form_service(booking_start_date, booking_start_hour) VALUES (:leconDate, :leconHeure)";
 
             $insert_prep = $connect->prepare($insertion_dateHourParking);
-            $insert_prep2 = $connect->prepare($insesrtion_dateHourAvi);
+            $insert_prepAvi = $connect->prepare($insertion_dateHourAvi);
+            $insert_prepAtt = $connect->prepare($insertion_dateHourAtt);
+            $insert_prepNet = $connect->prepare($insertion_dateHourNet);
+            $insert_prepPara = $connect->prepare($insertion_dateHourPara);
+            $insert_prepUlm = $connect->prepare($insertion_dateHourUlm);
+            $insert_prepBapt = $connect->prepare($insertion_dateHourBapt);
+            $insert_prepLecon = $connect->prepare($insertion_dateHourLecon);
 
             $insert_exec = $insert_prep->execute(array(':startDate'=>$startDate, ':endDate'=>$endDate, ':startHour'=>$startHour, ':endHour'=>$endHour));
-            $insert_exec2 = $insert_prep2->execute(array(':aviDate'=>$aviDate, ':aviHeure'=>$aviHeure));
+            $insert_execAvi = $insert_prepAvi->execute(array(':aviDate'=>$aviDate, ':aviHeure'=>$aviHeure));
+            $insert_execAtt = $insert_prepAtt->execute(array(':attDate'=>$attDate, ':attHeure'=>$attHeure));
+            $insert_execNet = $insert_prepNet->execute(array(':netDate'=>$netDate, ':netHeure'=>$netHeure));
+            $insert_execPara = $insert_prepPara->execute(array(':paraDate'=>$paraDate, ':paraHeure'=>$paraHeure));
+            $insert_execUlm = $insert_prepUlm->execute(array(':ulmDate'=>$ulmDate, ':ulmHeure'=>$ulmHeure));
+            $insert_execBapt = $insert_prepBapt->execute(array(':baptDate'=>$baptDate, ':baptHeure'=>$baptHeure));
+            $insert_execLecon = $insert_prepLecon->execute(array(':leconDate'=>$leconDate, 'leconHeure'=>$leconHeure));
            
-           $insert_prep = $connect->prepare($insertion);
+            $insert_prep = $connect->prepare($insertion);
             $insert_prep_acousticGroup = $connect->prepare($insertion_acousticGroup);
-            
+
 
             $inser_exec = $insert_prep->execute(array(':plane'=>$plane, ':fuel'=>$fuel, ':category'=>$category, ':planeLength'=>$planeLength, ':maxWeight'=>$maxWeight));
             $inser_exec_acousticGroup = $insert_prep_acousticGroup->execute(array(':acousticGroup'=>$acousticGroup));
@@ -88,6 +122,7 @@ if(isset($_POST['planeSelecter']) && isset($_POST['fuel']) && isset($_POST['cate
     }else{
         echo 'Tous les champs doivent être renseignés';
     }
+}
 
 
 ?>
