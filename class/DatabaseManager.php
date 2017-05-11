8fa91637007e8089c24410ca5bda1f1fff7c8cf1
lukile <lucile.1988.ls@gmail.com>
2017-05-11 17:18:36 +0200

    DatabaseManager added

---
+++ class/DatabaseManager.php
@@ -0,0 +1,47 @@
+<?php 
+
+class DatabaseManager{
+    private $connect;
+    private static $sharedInstance_;
+
+
+    public function __construct(){
+        $hostname = "localhost";
+        $database = "aen";
+        $username = "root";
+        $password = "";
+        
+        try{
+        $this->connect = new PDO("mysql:host=".$hostname.";dbname=".$database, $username, $password);
+        }catch(PDOException $e){
+            exit('problème de connexion à la base');
+            print_r($e);
+        }
+    }      
+
+    public function connect(){
+        $hostname = "localhost";
+        $database = "aen";
+        $username = "root";
+        $password = "";
+        
+       return new PDO("mysql:host=".$hostname.";dbname=".$database, $username, $password);
+    
+    }
+    public static function getSharedInstance(){
+        if(!isset(DatabaseManager::$sharedInstance_)){
+            DatabaseManager::$sharedInstance_ = new DatabaseManager();
+        }
+        return DatabaseManager::$sharedInstance_;
+    }
+
+        public function exec($sql, $params){
+        $statement = $this->connect->prepare($sql);
+        if($statement && $statement->execute($params)){
+            return true;
+        }
+            return false;
+    }
+}
+
+?>
