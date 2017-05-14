412a9b2eeb9b24e632244e8e82339957d3b1770a
lukile <lucile.1988.ls@gmail.com>
2017-05-14 03:30:21 +0200

    creation a session for activation key on a specific user

--- connexion.php
+++ connexion.php
@@ -1,57 +1,49 @@
 <?php
   
     require_once(__DIR__. '/class/DatabaseManager.php');
+    /*require("function.php");*/
 
     session_start();
 
     $error_msg = null;
     $message = null;
 
-    $pseudo = filter_input(INPUT_POST, 'pseudo');
+    $mail = filter_input(INPUT_POST, 'mail');
     $pass = filter_input(INPUT_POST, 'pass');
 
-    if(isset($pseudo, $pass)){
-        $pseudo = trim($pseudo) != '' ? $pseudo : null;
+    if(isset($mail, $pass)){
+        $mail = trim($mail) != '' ? $mail : null;
         $pass = trim($pass) != '' ? $pass : null;
 
-        if(isset($pseudo, $pass)){
-               /*$hostname = "localhost";
-               $database = "aen";
-               $username = "root";
-               $password = "";
-
-               $pdo_options[PDO::ATTR_EMULATE_PREPARES] = false;
-               $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-               $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
- 
-               try{
-                    $connect = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password, $pdo_options);
-               }catch (PDOException $e){
-                    exit('problème de connexion à la base');
-               }
-*/
+        if(isset($mail, $pass)){
+  
             $manager = DatabaseManager::getsharedInstance();
 
-            $requete = "SELECT id, pseudo, pass FROM user WHERE pseudo = :pseudo";
+            $requete = "SELECT id, mail, pass, activation_key FROM user WHERE mail = :mail";
             $connect = $manager->connect();
             $query = $connect->prepare($requete);
-            $query->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
+            $query->bindValue(':mail', $_POST['mail'], PDO::PARAM_STR);
             $query->execute();
             $data = $query->fetch();    
 
         
             if($data['pass'] == md5($_POST['pass'])){
-                $_SESSION['pseudo'] = $data['pseudo'];
+                $_SESSION['mail'] = $data['mail'];
+                $_SESSION['activation_key'] = $data['activation_key'];
                 $_SESSION['id'] = $data['id'];
-                $message = '<p> Bienvenue '.$data['pseudo'].', vous êtes maintenant connecté !';
+                $message = '<p> Bienvenue '.$data['mail']. ' '.$data['activation_key'].', vous êtes maintenant connecté !';
             }else{
-                $message = 'Le pseudo et/ou le mot de passe est incorrect';
+                $message = 'Le mail et/ou le mot de passe est incorrect';
             }
             $query->CloseCursor();
         }else{
             $message = 'Tous les champs doivent être renseignés';
         }
+
+        if(isConnected()){
+            echo 'ok';
+        }else{
+            echo 'nope';
+        }
     }
 ?>
-
-
