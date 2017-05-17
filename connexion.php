3999f83f08d03062959559b8f9ecdf86e741dbf3
lukile <lucile.1988.ls@gmail.com>
2017-05-17 23:31:05 +0200

    Including session start

--- connexion.php
+++ connexion.php
@@ -1,9 +1,7 @@
 <?php
   
     require_once(__DIR__. '/class/DatabaseManager.php');
-    /*require("function.php");*/
-
-    session_start();
+    /*include("function.php");*/
 
     $error_msg = null;
     $message = null;
@@ -16,34 +14,11 @@
         $pass = trim($pass) != '' ? $pass : null;
 
         if(isset($mail, $pass)){
-  
-            $manager = DatabaseManager::getsharedInstance();
-
-            $requete = "SELECT id, mail, pass, activation_key FROM user WHERE mail = :mail";
-            $connect = $manager->connect();
-            $query = $connect->prepare($requete);
-            $query->bindValue(':mail', $_POST['mail'], PDO::PARAM_STR);
-            $query->execute();
-            $data = $query->fetch();    
-
-        
-            if($data['pass'] == md5($_POST['pass'])){
-                $_SESSION['mail'] = $data['mail'];
-                $_SESSION['activation_key'] = $data['activation_key'];
-                $_SESSION['id'] = $data['id'];
-                $message = '<p> Bienvenue '.$data['mail']. ' '.$data['activation_key'].', vous êtes maintenant connecté !';
+            if(login($_POST['mail'], $_POST["pass"])){
+                $message = 'Vous êtes maintenant connecté !';
             }else{
-                $message = 'Le mail et/ou le mot de passe est incorrect';
+                $message = 'La connexion a echoué. Veuillez réessayer ultérieurement';
             }
-            $query->CloseCursor();
-        }else{
-            $message = 'Tous les champs doivent être renseignés';
-        }
-
-        if(isConnected()){
-            echo 'ok';
-        }else{
-            echo 'nope';
         }
     }
 ?>
