1969cb0596938794388a73add577ad4814bc86be
lukile <lucile.1988.ls@gmail.com>
2017-05-28 01:20:08 +0200

    Minor changes, display errors

--- inscription.php
+++ inscription.php
@@ -1,11 +1,14 @@
 <?php  
     session_start();
 
+    ini_set('display_errors', 1);
+
     header('Content-type: text/html; charset=UTF-8');
 
     include "header.php";
     include "createInscription.php";
 ?>    
+
     <!-- Page Content -->
     <div class="container">
 
@@ -28,14 +31,14 @@
     
 
 <div class="main-login main-center">
-                    <form class="form-horizontal" method="POST" action="#">
+                    <form class="form-horizontal" method="POST" action="#" name="formInscription">
                         
                         <div class="form-group">
                             <label for="firstname" class="cols-sm-2 control-label">Prénom</label>
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                                    <input type="text" class="form-control" name="firstname" id="firstname"  placeholder="Prénom"/>
+                                    <input type="text" class="form-control" name="firstname" id="firstname"  placeholder="Prénom" required/>
                                 </div>
                             </div>
                         </div>
@@ -45,7 +48,7 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                                    <input type="text" class="form-control" name="lastname" id="lastname"  placeholder="Nom"/>
+                                    <input type="text" class="form-control" name="lastname" id="lastname"  placeholder="Nom" required/>
                                 </div>
                             </div>
                         </div>
@@ -55,7 +58,7 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-                                    <input type="text" class="form-control" name="pseudo" id="pseudo"  placeholder="Nom d'utilisateur"/>
+                                    <input type="text" class="form-control" name="pseudo" id="pseudo"  placeholder="Nom d'utilisateur" required/>
                                 </div>
                             </div>
                         </div>
@@ -65,7 +68,8 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                    <input type="password" class="form-control" name="pass" id="pass"  placeholder="Saisir le mot de passe"/>
+                                    <input type="password" class="form-control" name="pass" id="pass"  placeholder="Saisir le mot de passe" required onBlur="verifyPassLength()"/><br/>
+                                        <div id="msgLength"></div>
                                 </div>
                             </div>
                         </div>
@@ -75,7 +79,8 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                    <input type="password" class="form-control" name="pass_validation" id="pass_validation"  placeholder="Confirmer le mot de passe"/>
+                                    <input type="password" class="form-control" name="pass_validation" id="pass_validation" placeholder="Confirmer le mot de passe" required onBlur="verifyPassAgreement()"/>
+                                    <div id="msgAgreement"></div>
                                 </div>
                             </div>
                         </div>
@@ -85,7 +90,8 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                    <input type="text" class="form-control" name="mail" id="mail"  placeholder="Email"/>
+                                    <input type="text" class="form-control" name="mail" id="mail"  placeholder="Email" required onBlur="validMail(this)"/>
+                                    <div id="msgMail"></div>
                                 </div>
                             </div>
                         </div>     
@@ -95,7 +101,7 @@
                             <div class="cols-sm-10">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-phone fa-lg" aria-hidden="true"></i></span>
-                                    <input type="text" class="form-control" name="phone" id="phone"  placeholder="Numéro de téléphone"/>
+                                    <input type="text" class="form-control" name="phone" id="phone" required placeholder="Numéro de téléphone"/>
                                 </div>
                             </div>
                         </div>
