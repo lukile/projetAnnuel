c529c112220b65b1996e44772523d231f6483350
lukile <lucile.1988.ls@gmail.com>
2017-05-28 01:16:19 +0200

    Try to send fake localhost email

--- createInscription.php
+++ createInscription.php
@@ -1,4 +1,7 @@
 <?php
+ini_set("SMTP","smtp.gmail.com");
+ini_set("smtp_port","25");
+
 /* Indique le bon format des entêtes (par défaut apache risque de les envoyer au standard ISO-8859-1)*/
 
 /* Initialisation de la variable du message de réponse*/
@@ -10,8 +13,8 @@ $lastname = filter_input(INPUT_POST, 'lastname');
 $firstname = filter_input(INPUT_POST, 'firstname');
 $mail = filter_input(INPUT_POST, 'mail');
 $pseudo = filter_input(INPUT_POST, 'pseudo');
-$pass = md5(filter_input(INPUT_POST, 'pass'));
-$pass_validation = md5(filter_input(INPUT_POST, 'pass_validation'));
+$pass = filter_input(INPUT_POST, 'pass');
+$pass_validation = filter_input(INPUT_POST, 'pass_validation');
 $phone = filter_input(INPUT_POST, 'phone');
 $comments = filter_input(INPUT_POST, 'comments');
 
@@ -51,7 +54,57 @@ if (isset($lastname, $firstname, $mail, $pseudo, $pass, $pass_validation, $phone
                                              $_POST['comments']); 
 
                             InscriptionDAO::create($user);
-                                                        
+$mail = 'lucile.1988.ls@gmail.com'; // Déclaration de l'adresse de destination.
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+//=====Déclaration des messages au format texte et au format HTML.
+$message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+$message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
+//==========
+ 
+//=====Création de la boundary
+$boundary = "-----=".md5(rand());
+//==========
+ 
+//=====Définition du sujet.
+$sujet = "Hey mon ami !";
+//=========
+ 
+//=====Création du header de l'e-mail.
+$header = "From: \"WeaponsB\"<lucile.1988.ls@gmail.com>".$passage_ligne;
+$header.= "Reply-to: \"WeaponsB\" <lucile.1988.ls@gmail.com>".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+ 
+//=====Création du message.
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format texte.
+$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_txt.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format HTML
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+ 
+//=====Envoi de l'e-mail.
+mail($mail,$sujet,$message,$header);
+//==========
+                            
+                             
                         }else{
                             $message = 'Les deux mots de passe doivent être identiques';
                         }
