1b6bdf4f3f109965fe34dde18f37e8f53e2c4cae
lukile <lucile.1988.ls@gmail.com>
2017-06-08 14:20:30 +0200

    Made function to insert values

--- function.php
+++ function.php
@@ -76,6 +76,37 @@ function logout(){
     header("Location:index.php");
 }
 
+function validate($startDate){
+    $dateFormat = date_create_from_format('d-m-Y', $startDate);
+    $formattedDate = $dateFormat->format('Y-m-d');
+
+    if($formattedDate < strftime('%Y-%m-%d')){
+        $message = 'La date de réservation ne peut s\'effectuer avant la date courante';   
+        printf($message);
+        return false;
+    }
+    return true;
+}
+
+function insertServiceValues($startDate, $startHour, $orderFormId, $serviceId){
+    $manager = DatabaseManager::getsharedInstance();
+    $connect = $manager->connect(); 
+   
+    $select = "INSERT INTO order_form_service(booking_start_date, booking_start_hour, order_form_id, service_id) VALUES(:startDate, :startHour, :orderFormId, :serviceId)";
+    $prep = $connect->prepare($select);
+    $exec = $prep->execute(array(':startDate'=>$startDate, ':startHour'=>$startHour, ':orderFormId'=>$orderFormId, ':serviceId'=>$serviceId));
+    echo 'insertion ok';
+    return $exec;
+}
+
+function insertRoyalties($plane, $fuel, $category, $planeLength, $maxWeight, $idService, $acousticGroup){
+    $manager = DatabaseManager::getsharedInstance();
+    $connect = $manager->connect();
+
+    $insert = "INSERT INTO royalties(landing_type, petroleum_type, rate_type, plane_length, plane_weight, service_id, acoustic_group) VALUES(:plane, :fuel, :category, :planeLength, :maxWeight, :idService, :acoustic_group)";
+    $insert_prep = $connect->prepare($insert);
+    $insert_exec = $insert_prep->execute(array(':plane'=>$plane, ':fuel'=>$fuel, ':category'=>$category, ':planeLength'=>$planeLength, ':maxWeight'=>$maxWeight,'idService'=>$idService,':acoustic_group'=>$acousticGroup));
+}
 function sendMail(){
 
 
