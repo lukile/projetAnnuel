c66981ddd2eb582e3df1c0ddb3becc06afb61808
SachaB96 <sacha0007@msn.com>
2017-06-10 16:15:38 +0200

    cant doing it better for now

--- validationemail.php
+++ validationemail.php
@@ -1,5 +1,6 @@
 <?php
 	require_once(__DIR__. '/class/DatabaseManager.php');
+	include "header.php";
 
             $hostname = "localhost";
             $database = "aen";
@@ -12,28 +13,58 @@ try{
         exit('problème de connexion à la base');
       }
 
-if( !isset($_GET['activationKey'])){
+ if( !isset($_GET['activationKey']) && !isset($_GET['mail'])){
 
 	die("Pas de token");
 
 }
 $activationKey = $_GET['activationKey'];
 
-$query = $connect->prepare("SELECT id FROM user WHERE activation_key='$activationKey'");
+$mail = $_GET['mail'];
+//echo $activationKey;
+//echo $mail;
+
+$query = $connect->prepare("SELECT id FROM user WHERE mail='$mail'");
 
 //Executer et récupérer l'information
-$query->execute(["activationKey"=>$_GET['activationKey']]);
+$query->execute(["mail"=>$_GET['mail']]);
 $results = $query->fetch();
 
 if( !empty($results) ){
 	//il existe on prépare une autre requête
-	$query = $connect->prepare("UPDATE user SET active = 1 WHERE activation_key='$activationKey'");
+	$query = $connect->prepare("UPDATE user SET active = 1 WHERE mail='$mail'");
 	//On execute la nouvelle requete
-	$query->execute(["activation_key"=>$_GET['activationKey']]);
-	header("Location: index.php");
+	$query->execute(["mail"=>$_GET['mail']]);
 
 }else{
 	die("Erreur a la fin, bdd pas mise a jour");
 }
+?>
+<div class="container">
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Activation de compte
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Accueil</a>
+                    </li>
+                    <li class="active">Activation reussie</li>
+                </ol>
+                <div>
+                    <h4>Votre compte est maintenant activé vous pouvez maintenant commencer à reserver des services, redirection vers la page d'accueil.</h4>
+                </div>
+            </div>
+        </div>
+                   
+<script>
+setTimeout("location.href='index.php';", 4000);
+</script>
+
+<?php
+
+include "footer.php";
+
+
 //et si il n'existe pas on die
 ?>
