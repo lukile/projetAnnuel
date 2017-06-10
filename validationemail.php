16724a7f849b1110103b05f6d964a79dc2af344d
SachaB96 <sacha0007@msn.com>
2017-06-10 15:53:35 +0200

    ativation by adress mail

--- validationemail.php
+++ validationemail.php
@@ -12,24 +12,27 @@ try{
         exit('problème de connexion à la base');
       }
 
-if( !isset($_GET['activationKey'])){
+if( !isset($_GET['activationKey']) && !isset($_GET['mail'])){
 
 	die("Pas de token");
 
 }
 $activationKey = $_GET['activationKey'];
+$mail = $_GET['mail'];
+echo $activationKey;
+echo $mail;
 
-$query = $connect->prepare("SELECT id FROM user WHERE activation_key='$activationKey'");
+$query = $connect->prepare("SELECT id FROM user WHERE mail='$mail'");
 
 //Executer et récupérer l'information
-$query->execute(["activationKey"=>$_GET['activationKey']]);
+$query->execute(["mail"=>$_GET['mail']]);
 $results = $query->fetch();
 
 if( !empty($results) ){
 	//il existe on prépare une autre requête
-	$query = $connect->prepare("UPDATE user SET active = 1 WHERE activation_key='$activationKey'");
+	$query = $connect->prepare("UPDATE user SET active = 1 WHERE mail='$mail'");
 	//On execute la nouvelle requete
-	$query->execute(["activation_key"=>$_GET['activationKey']]);
+	$query->execute(["mail"=>$_GET['mail']]);
 	header("Location: index.php");
 
 }else{
