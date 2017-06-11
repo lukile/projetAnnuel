b84aa9cc31e1ec98c50c0bb80907424d2e6d110e
lukile <lucile.1988.ls@gmail.com>
2017-06-11 17:51:04 +0200

    admin feature list

---
+++ admin.php
@@ -0,0 +1,75 @@
+<?php 
+    session_start();
+    include "header.php";
+?>    
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">ADMIN
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Accueil</a>
+                    </li>
+                    <li class="active">Admin</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <li><a href="listUser.php">Affichage des utilisateurs</a></li>
+                            </h4>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <li><a href = "createUser.pho">Créer un utilisateur</a>
+                            </h4>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <li><a href = "modify.php">Modifier un utilisateur</a>
+                            </h4>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.panel-group -->
+            </div>
+            <!-- /.col-lg-12 -->
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+<?php 
+    include "footer.php";
+?>   
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
