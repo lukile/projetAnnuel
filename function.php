175f10c2f16ae772cc6db3618ef8900a2d1a29b8
lukile <lucile.1988.ls@gmail.com>
2017-06-11 18:02:16 +0200

    added factorisation functions

--- function.php
+++ function.php
@@ -1,12 +1,17 @@
 <?php 
 require_once(__DIR__. "/class/DatabaseManager.php");
 
+function connect(){
+    $manager = DatabaseManager::getsharedInstance();
+    $connect = $manager->connect();
+
+    return $connect;
+}
+
 function isConnected(){
     if(!empty($_SESSION["activation_key"]) && !empty($_SESSION["mail"])){
-        $manager = DatabaseManager::getsharedInstance();
-        $connect = $manager->connect();
         
-        $query = $connect->prepare("SELECT id FROM user where mail=:mail AND activation_key=:activation_key");
+        $query = connect()->prepare("SELECT id FROM user where mail=:mail AND activation_key=:activation_key");
         $query->execute(["mail" => $_SESSION['mail'], "activation_key" => $_SESSION['activation_key']]);
         $result = $query->fetch();
 
@@ -23,10 +28,8 @@ function isConnected(){
 }
 
 function getActivationKey(){
-    $manager = DatabaseManager::getsharedInstance();
-    $connect = $manager->connect();
 
-    $query = $connect->prepare("SELECT activation_key WHERE mail=:mail AND activation_key=:activation_key");
+    $query = connect()->prepare("SELECT activation_key WHERE mail=:mail AND activation_key=:activation_key");
     $query->execute(["mail"=> $_SESSION['mail'], "activation_key"=>$_SESSION['activation_key']]);
     $result = $query->fetch();
 
@@ -38,10 +41,8 @@ function getActivationKey(){
 }
 
 function login($mail, $pass){
-    $manager = DatabaseManager::getsharedInstance();
-    $connect = $manager->connect();
 
-    $query = $connect->prepare("SELECT pass FROM user WHERE mail=:mail");
+    $query = connect()->prepare("SELECT pass FROM user WHERE mail=:mail");
     $query->execute(["mail"=>$mail]);
     $result = $query->fetch();
     $pass = md5($pass);
@@ -60,10 +61,7 @@ function login($mail, $pass){
 function generateactivation_key($mail){
     $activation_key = md5(uniqid());
 
-    $manager = DatabaseManager::getsharedInstance();
-    $connect = $manager->connect();
-
-    $query = $connect->prepare('UPDATE user SET activation_key=:activation_key WHERE mail=:mail');
+    $query = connect()->prepare('UPDATE user SET activation_key=:activation_key WHERE mail=:mail');
 
     $query->execute(["activation_key"=>$activation_key, "mail"=>$mail]);
 
@@ -79,7 +77,6 @@ function logout(){
 function validate($startDate){
     $dateFormat = date_create_from_format('d-m-Y', $startDate);
     $formattedDate = $dateFormat->format('Y-m-d');
-
     if($formattedDate < strftime('%Y-%m-%d')){
         $message = 'La date de réservation ne peut s\'effectuer avant la date courante';   
         printf($message);
@@ -98,18 +95,33 @@ function insertServiceValues($startDate, $startHour, $orderFormId, $serviceId){
     echo 'insertion ok';
     return $exec;
 }
-
 function insertRoyalties($plane, $fuel, $category, $planeLength, $maxWeight, $idService, $acousticGroup){
     $manager = DatabaseManager::getsharedInstance();
     $connect = $manager->connect();
-
     $insert = "INSERT INTO royalties(landing_type, petroleum_type, rate_type, plane_length, plane_weight, service_id, acoustic_group) VALUES(:plane, :fuel, :category, :planeLength, :maxWeight, :idService, :acoustic_group)";
     $insert_prep = $connect->prepare($insert);
     $insert_exec = $insert_prep->execute(array(':plane'=>$plane, ':fuel'=>$fuel, ':category'=>$category, ':planeLength'=>$planeLength, ':maxWeight'=>$maxWeight,'idService'=>$idService,':acoustic_group'=>$acousticGroup));
 }
-function sendMail(){
 
 
+
+function displayListUsers(){
+  $listUsers = connect()->query("SELECT firstname, lastname, pseudo, mail, phone, comments, registration_date, application_fee FROM user");
+
+    while($data = $listUsers->fetch()){
+        echo '<tr> <td>'.$data['firstname'].'</td>';
+        echo '<td>'.$data['lastname'].'</td>';
+        echo '<td>'.$data['pseudo'].'</td>';
+        echo '<td>'.$data['mail'].'</td>';
+        echo '<td>'.$data['phone'].'</td>';
+        echo '<td>'.$data['comments'].'</td>';
+        echo '<td>'.$data['registration_date'].'</td>';
+        echo '<td>'.$data['application_fee'].'</td> </tr>';
+    }  
 }
+
+
+
+
 
 ?>
