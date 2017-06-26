f155c4e4a5a6d5f9e6c76d9ca54bd4a3119ccdbe
lukile <lucile.1988.ls@gmail.com>
2017-06-26 21:21:01 +0200

    Updated Royalty class

--- class/Royalty.php
+++ class/Royalty.php
@@ -1,142 +1,111 @@
 <?php 
     class Royalty{
-        private $id_;
-        private $petroleumProduct_;
-        private $shelterFeature_;
-        private $landingType_;
-        private $landingPeriod_;
-        private $timeUnit_;
-        private $parkingSurface_;
-        private $parkingWeek_;
-        private $rate_;
-        private $rateType_;
-        private $htPrice_;
-        private $ttcPrice_;
-        private $tvaPrice_;
-        private $serviceId_;
-        private $coeffAcousticGroup_;
+        private $id;
+        private $petroleumProduct;
+        private $plan;
+        private $qteFuel;
+        private $category;
+        private $planLength;
+        private $maxWeight;
+        private $planWidth;
+        private $surface;
+        private $htPrice;
+        private $ttcPrice;
+        private $ffa;
+        private $serviceId;
+        private $coeffAcousticGroup;
 
-        public function __construct($id, $petroleumProduct, $shelterFeature, $landingType, $landingPeriod, $timeUnit, $parkingSurface, $parkingWeek, $rate, $rateType, $htPrice, $ttcPrice, $tvaPrice, $serviceId, $coeffAcousticGroup){
-            $this->id_ = $id;
-            $this->petroleumProduct_ = $petroleumProduct;
-            $this->shelterFeature_ = $shelterFeature;
-            $this->landingType_ = $landingType;
-            $this->landingPeriod_ = $landingPeriod;
-            $this->timeUnit_ = $timeUnit;
-            $this->parkingSurface_ = $parkingSurface;
-            $this->parkingWeek_ = $parkingWeek;
-            $this->rate_ = $rate;
-            $this->rateType_ = $rateType;
-            $this->htPrice_ = $htPrice;
-            $this->ttcPrice_ = $ttcPrice;
-            $this->tvaPrice_ = $tvaPrice;
-            $this->serviceId_ = $serviceId;
-            $this->coeffAcousticGroup_ = $coeffAcousticGroup;
+        public function __construct($petroleumProduct, $plan, 
+                                    $qteFuel, $category, $planLength, 
+                                    $maxWeight, $planWidth, $surface,
+                                    $htPrice, $ttcPrice, $ffa, $serviceId, $coeffAcousticGroup){
+            $this->petroleumProduct = $petroleumProduct;
+            $this->plan = $plan;
+            $this->qteFuel = $qteFuel;
+            $this->category = $category;
+            $this->planLength = $planLength;
+            $this->maxWeight = $maxWeight;
+            $this->planWidth = $planWidth;
+            $this->surface = $surface;
+            $this->htPrice = $htPrice;
+            $this->ttcPrice = $ttcPrice;
+            $this->ffa = $ffa;
+            $this->serviceId = $serviceId;
+            $this->coeffAcousticGroup = $coeffAcousticGroup;
         }
 
         public function getId(){
-            return $this->id_;
+            return $this->id;
         }
         public function setId($id){
-            $this->id_ = $id;
+            $this->id = $id;
         }
 
         public function getPetroleumProduct(){
-            return $this->petroleumProduct_;
-        }
-        public function setPetroleumProduct($petroleumProduct){
-            $this->petroleumProduct_ = $petroleumProduct;
+            return $this->petroleumProduct;
         }
 
-        public function getShelterFeature(){
-            return $this->shelterFeature_;
-        }
-        public function setShelterFeature($shelterFeature){
-            $this->shelterFeature = $shelterFeature;
+        public function getPlan(){
+            return $this->plan;
         }
         
-        public function getLandingType(){
-            return $this->landingType_;
-        }
-        public function setLandingType($landingType){
-            $this->landingType_ = $landingType;
+        public function getQteFuel(){
+            return $this->qteFuel;
         }
 
-        public function getLandingPeriod(){
-            return $this->landingPeriod_;
-        }
-        public function setLandingPeriod($landingPeriod){
-            $this->landingPeriod_ = $landingPeriod;
+        public function getCategory(){
+            return $this->category ;
         }
 
-        public function getTimeUnit(){
-            return $this->timeUnit_;
-        }
-        public function setTimeUnit($timeUnit){
-            $this->timeUnit_ = $timeUnit;
+        public function getPlanLength(){
+            return $this->planLength;
         }
 
-        public function getParkingSurface(){
-            return $this->parkingSurface_;
+        public function getMaxWeight(){
+            return $this->maxWeight;
         }
-        public function setParkingSurface($parkingSurface){
-            $this->parkingSurface_ = $parkingSurface;
+    
+        public function getPlanWidth(){
+            return $this->planWidth;
         }
-        
-        public function getParkingWeek(){
-            return $this->parkingWeek_;
+         
+        public function getSurface(){
+            return $this->surface;
         }
-        public function setParkingWeek($parkingWeek){
-            $this->parkingWeek_ = $parkingWeek;
-        }
-
-        public function getRate(){
-            return $this->rate_;
-        }
-        public function setRate($rate){
-            $this->rate_ = $rate;
-        }
-
-        public function getRateType(){
-            return $this->rateType_;
-        }
-        public function setRateType($rateType){
-            $this->rateType_ = $rateType;
-        }
-
         public function getHtPrice(){
-            return $this->htPrice_;
-        }
-        public function setHtPrice($htPrice){
-            $this->htPrice_ = $htPrice;
+            return $this->htPrice;
         }
 
         public function getTtcPrice(){
-            return $this->ttcPrice_;
+            return $this->ttcPrice;
         }
-        public function setTtcPrice($ttcPrice){
-            $this->ttcPrice_ = $ttcPrice;
-        }
-
-        public function getTvaPrice(){
-            return $this->tvaPrice_;
-        }
-        public function setTvaPrice($tvaPrice){
-            $this->tvaPrice_ = $tvaPrice;
+        
+        public function getFfa(){
+            return $this->ffa;
         }
 
         public function getServiceId(){
-            return $this->serviceId_;
-        }
-        public function setServiceId($serviceId){
-            $this->serviceId_ = $serviceId;
+            return $this->serviceId;
         }
 
         public function getCoeffAcousticGroup(){
-            return $this->coeffAcousticGroup_;
+            return $this->coeffAcousticGroup;
         }
+
+        public function setHtPrice($htPrice){
+            $this->htPrice = $htPrice;
+        }
+
+        public function setTtcPrice($ttcPrice){
+            $this->ttcPrice = $ttcPrice;
+        }
+
+        public function setServiceId($serviceId){
+            $this->serviceId = $serviceId;
+        }
+
         public function setCoeffAcousticGroup($coeffAcousticGroup){
-            $this->coeffAcousticGroup_ = $coeffAcousticGroup;
+            $this->coeffAcousticGroup = $coeffAcousticGroup;
         }
     }
 ?>
