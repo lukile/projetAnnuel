d3ea5bc569e9e045a3ea777378af2996177d6b22
lukile <lucile.1988.ls@gmail.com>
2017-06-26 21:31:00 +0200

    message tu put with invoice

---
+++ conditionInvoice.php
@@ -0,0 +1,25 @@
+<?php
+Information :
+
+Ceci est une estimation de votre facture, elle ne constitue en aucun cas le prix définitif des
+prestations.
+En effet, des frais supplémentaires peuvent être engendrés notamment dans les cas suivants :
+
+- Frais de dossiers : le total de la facture impayée le jour du mouvement est majoré de 31€
+    (25,83€ HTVA), pour frais de recherches et de dossiers.
+
+- La licence et l'assurance n'ont pas été au préalable renseignées lors de la réservation.
+    Pour rappel, les frais s'élèvent à 74€ pour la licence + assurance et à 114€ pour la 
+    licence + l'assurance + revue mensuelle "Info pilote".
+
+- Les frais de cotisation à l'aéroclub ne sont pas compris dans le devis de la facture.
+    Elle donne accès aux locaux et aux avions.
+    Ainsi, vous devrez vous acquitter de 218€, le jour de votre arrivée pour toute activités
+    relatives à l'aéroclub et 178€ si vous avez moins de 21 ans ou que vous êtes membres 
+    d'un autre aéroclub.
+    La cotisation est valable pour une année civile pleine
+
+- Si vous avez choisit l'activité "Nettoyage intérieur", compte tenu de la variabilité des
+    prix, ceux ci seront réglés le jour de l'activité, sur place.
+
+?>
