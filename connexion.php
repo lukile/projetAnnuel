3448183c98c5d9ae32671b61946c4747900f55c2
lukile <lucile.1988.ls@gmail.com>
2017-06-26 21:23:03 +0200

    Refacto login method

--- connexion.php
+++ connexion.php
@@ -1,35 +1,30 @@
 <?php
 
-    require_once(__DIR__. '/class/DatabaseManager.php');
+    require_once(__DIR__. '/class/LoginManager.php');
     /*include("function.php");*/
 
     $error_msg = null;
     $message = null;
 
     $mail = filter_input(INPUT_POST, 'mail');
-    $pass = md5(filter_input(INPUT_POST, 'pass'));
+    $pass = filter_input(INPUT_POST, 'pass');
 
-    if(isset($mail, $pass)){
-        $mail = trim($mail) != '' ? $mail : null;
-        $pass = trim($pass) != '' ? $pass : null;
-
-        if(isset($mail, $pass)){
-            
-            if(login($_POST['mail'], $_POST['pass'])){
-                $message = 'Vous êtes maintenant connecté '.$_POST['mail'];
-
-                ?>
-                <script>
-                    setTimeout("location.href='index.php';", 500);
-                </script>
-                
-                <?php    
-
-            }else{
-                $message = 'Vous avez saisi les mauvais identifiants !';
-            }
-        }else{
-            $message = 'Veuillez renseigner tous les champs s\'il vous plaît';
-        }
+    if(!isset($mail, $pass)) {
+        $message = 'Veuillez renseigner tous les champs s\'il vous plaît';
+        return;
     }
-?>
+
+    $mail = trim($mail) != '' ? $mail : null;
+    $pass = trim($pass) != '' ? $pass : null;
+
+    $loginManager = LoginManager::getSharedInstance();
+    if (! $loginManager->login($mail, $pass)) {
+        $message = 'Vous avez saisi les mauvais identifiants !';
+        return;
+    }
+
+    $message = 'Vous êtes maintenant connecté '.$mail;
+    ?>
+    <script>
+        setTimeout("location.href='index.php';", 500);
+    </script>
