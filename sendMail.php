2262dddf9525c36d4d0df7ea243b282e11e9898c
lukile <lucile.1988.ls@gmail.com>
2017-06-30 15:56:59 +0200

    Minor changes

--- sendMail.php
+++ sendMail.php
@@ -1,19 +1,20 @@
 <?php 
 //session_start();
 //include "function.php";
-//include "checkUser.php";
+//include "checkUser.php"
+require_once("function.php");
 if(isset($_GET['id']) && isset($_GET['mail'])){
 		$id = $_GET['id'];
 		$email = $_GET['mail'];
 
 
-    $fetch_id = connect()->prepare("SELECT COUNT(of.id) FROM order_form of WHERE of.id in (SELECT ofs.order_form_id FROM order_form_service ofs WHERE ofs WHERE ofs.service_id = 10) AND of.user_id = :id");
+    $fetch_id = connect()->prepare("SELECT COUNT(of.id) FROM order_form of WHERE of.id in (SELECT ofs.order_form_id FROM order_form_service ofs WHERE ofs.service_id = 10) AND of.user_id = :id");
     $fetch_id->execute([':id'=>$_GET['id']]);
     $result = $fetch_id->fetch();
 
-    if ( $result[0] == 10 ) {
-     echo "ok";
-    // $link="<a href='http://localhost/projet_annuel/reset.php?key=".$email."&reset=".$pass."'>Cliquez pour renitialiser votre mot de passe</a>";
+    if ( $result[0] == 10) {
+     echo "Mail envoyé à ".$email;
+     $link="<a href='http://localhost/projet_annuel/contact.php'>Contact support</a>";
     
     require_once('phpmailer/PHPMailerAutoload.php');
 
@@ -31,13 +32,12 @@ if(isset($_GET['id']) && isset($_GET['mail'])){
     $mail->AddAddress($email,$email);
     $mail->Subject  =  'Présentation a l"examen';
     $mail->IsHTML(true);
-    $mail->Body    = 'Bonjour à vous, vous avez cumulé plus de 10 cours en leçon de pilotage, nous vous proposons donc de passer un examen afin de recevoir au plus rapidement votre titre confimé.';
+    $mail->Body    = 'Bonjour,<br/> Vous avez cumulé plus de 10 cours de leçon de pilotage, <br/> nous vous proposons donc de passer un examen afin de recevoir au plus rapidement votre titre confimé. <br/> Contactez l\'AEN pour prendre rendez vous<br/>'.$link ;
     $mail->Send();
      return true;
-     
         
     } else {
-    	 echo "pas bon";
+    	 echo "Le client n'as pas cumulé plus de 10 cours, il n'a pas accès au passage d'examens";
         return false;
        
     }
